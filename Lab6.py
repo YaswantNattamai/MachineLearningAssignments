fe5249000d661c23adcc313ba3156d2d6692c50a
YaswantNattamai <saiyaswantnattamai@gmail.com>
2025-08-24 04:32:52 +0530

    Lab 6 final

--- Lab6.py
+++ Lab6.py
@@ -5,9 +5,6 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
 # ================================ Question 1 & 2 ================================
